49fc452d7d79fcab3aff0fc59f5e36b099bad560
Marco Maggi <marco.maggi-ipsu@poste.it>
2014-01-02 15:47:15 +0100

    review of maintainer's script

--- configure.sh
+++ configure.sh
@@ -4,13 +4,17 @@
 set -ex
 
 prefix=/usr/local
-
+if test -d /lib64
+then libdir=${prefix}/lib64
+else libdir=${prefix}/lib
+fi
 ../configure \
     --config-cache				\
     --cache-file=../config.cache		\
     --enable-maintainer-mode                    \
     --disable-static --enable-shared            \
     --prefix="${prefix}"			\
+    --libdir="${libdir}"			\
     CFLAGS='-O3 -pedantic'			\
     "$@"
 
